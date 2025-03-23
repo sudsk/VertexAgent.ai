1b1ff61e360c898f10301949f1e42de7a88a5b94
Suds Kumar <suds123@gmail.com>
2025-03-23 17:29:55 +0000

    Update useQueryParams.js

--- src/hooks/useQueryParams.js
+++ src/hooks/useQueryParams.js
@@ -1,1 +1,36 @@
+// src/hooks/useQueryParams.js
+import { useLocation, useNavigate } from 'react-router-dom';
 
+export default function useQueryParams() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const queryParams = new URLSearchParams(location.search);
+  
+  const getQueryParam = (key) => {
+    return queryParams.get(key);
+  };
+  
+  const setQueryParam = (key, value) => {
+    if (value === null || value === undefined || value === '') {
+      queryParams.delete(key);
+    } else {
+      queryParams.set(key, value);
+    }
+    
+    navigate({
+      pathname: location.pathname,
+      search: queryParams.toString()
+    });
+  };
+  
+  const removeQueryParam = (key) => {
+    queryParams.delete(key);
+    
+    navigate({
+      pathname: location.pathname,
+      search: queryParams.toString()
+    });
+  };
+  
+  return { getQueryParam, setQueryParam, removeQueryParam };
+}
