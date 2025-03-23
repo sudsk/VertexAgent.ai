5a4c8d1963cb5eb482faff9aeab7731b6da8b108
Suds Kumar <suds123@gmail.com>
2025-03-23 17:56:10 +0000

    Create authService.js

---
+++ src/services/authService.js
@@ -0,0 +1,13 @@
+// src/services/authService.js
+// This is a mock file to allow the frontend to build
+// Authentication should be handled by the backend server
+
+export const getAuthenticatedClient = async () => {
+  console.warn('getAuthenticatedClient called on frontend - authentication should be done on backend');
+  return null;
+};
+
+export const getAccessToken = async () => {
+  console.warn('getAccessToken called on frontend - authentication should be done on backend');
+  return null;
+};
