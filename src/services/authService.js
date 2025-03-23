d86c7a6a879749f27652218205363e27ddd28b10
Suds Kumar <suds123@gmail.com>
2025-03-23 17:22:22 +0000

    Update authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,1 +1,17 @@
+import { GoogleAuth } from 'google-auth-library';
 
+// Configure authentication
+const auth = new GoogleAuth({
+  scopes: ['https://www.googleapis.com/auth/cloud-platform']
+});
+
+export const getAuthenticatedClient = async () => {
+  const client = await auth.getClient();
+  return client;
+};
+
+export const getAccessToken = async () => {
+  const client = await auth.getClient();
+  const tokenResponse = await client.getAccessToken();
+  return tokenResponse.token;
+};
