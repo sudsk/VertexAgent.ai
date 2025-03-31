b96be0892d4f7a947e8b00215cdaa01825653b08
Suds Kumar <suds123@gmail.com>
2025-03-31 21:07:51 +0100

    Update CustomCodeEditor.jsx

--- frontend/src/components/CustomCodeEditor.jsx
+++ frontend/src/components/CustomCodeEditor.jsx
@@ -82,7 +82,7 @@ const CustomCodeEditor = ({ framework, onChange, initialCode = {} }) => {
                 : 'text-gray-500 hover:text-gray-700 hover:border-gray-300'
             }`}
           >
-            <Tool size={16} className="inline mr-1" />
+            <Wrench size={16} className="inline mr-1" />
             Custom Tools
           </button>
           <button
